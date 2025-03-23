25c4c7a645a77eb0d27936e0bf44623aec640ee5
bslocombe <ben@techreative.com.au>
2025-03-23 15:50:43 +1100

    fix: remove Fibres and bump for meteor3

--- package.js
+++ package.js
@@ -1,19 +1,22 @@
 Package.describe({
-  name: 'bslocombe:pg-server',
+  name: "bslocombe:pg-server",
   // debugOnly: true,
-  version: '1.0.6',
-  summary: 'Run PostgreSQL server inside your Meteor app',
-  git: 'https://github.com/bslocombe/meteor-pg-server.git',
-  documentation: 'README.md'
+  version: "1.1.1",
+  summary: "Run PostgreSQL server inside your Meteor app",
+  git: "https://github.com/bslocombe/meteor-pg-server.git",
+  documentation: "README.md",
 });
 
 function determinePlatformNpmPackage() {
-  switch(process.platform + '_' + process.arch) {
+  switch (process.platform + "_" + process.arch) {
     // case 'linux_x64': return 'pg-server-9.4-linux-x64';
     // case 'linux_ia32': return 'pg-server-9.4-linux-i386';
-    case 'darwin_x64': return true;
-    case 'darwin_arm64': return true;
-    default: return null;
+    case "darwin_x64":
+      return true;
+    case "darwin_arm64":
+      return true;
+    default:
+      return null;
   }
 }
 
@@ -21,48 +24,38 @@ function determinePlatformNpmPackage() {
 // bcrypt is an npm package that
 // has different binaries for different architectures.
 Npm.depends({
-  bcrypt: '5.1.1',
-  "extend": "2.0.2",
-  "spawn-sync": "1.0.15"
+  bcrypt: "5.1.1",
+  extend: "2.0.2",
+  "spawn-sync": "1.0.15",
 });
 
 var npmPkg = determinePlatformNpmPackage();
 
-if(npmPkg === null) {
-  console.error('ERROR: Platform is not supported by slocombe:pg-server!');
-  console.error('       Supports only and OSX (64 bit, arm64)');
+if (npmPkg === null) {
+  console.error("ERROR: Platform is not supported by slocombe:pg-server!");
+  console.error("       Supports only and OSX (64 bit, arm64)");
 } else {
   var depend = {
     // For initialization queries
-    'pg': '8.0.3',
-    'spawn-sync': '1.0.15'
+    pg: "8.0.3",
+    "spawn-sync": "1.0.15",
   };
-  // removing binary dependencies
-  // platform dependent pg-server-xxx package
-  // if(npmPkg == "darwin_arm64"){
-    // depend[npmPkg] = 'https://github.com/bslocombe/pg-server-14.2-osx-arm64.git#master';
-  // }else{
-  //   depend[npmPkg] = '9.4.4'
-  // }
 
   Package.registerBuildPlugin({
-    name: 'pgServer',
-    use: [ 'underscore@1.0.3'],
-    sources: [
-      'plugin/pgServer.js'
-    ],
-    npmDependencies: depend
+    name: "pgServer",
+    use: ["underscore@1.0.3"],
+    sources: ["plugin/pgServer.js"],
+    npmDependencies: depend,
   });
 }
 
-Package.onUse(function(api) {
-  api.versionsFrom('2.5.1');
-  // api.use('isobuild:compiler-plugin@1.0.0');
+Package.onUse(function (api) {
+  api.versionsFrom("2.11.0", "3.0");
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('bslocombe:pg-server');
-  api.use('bslocombe:pg@1.0.4');
-  api.addFiles('pg-server-tests.js', 'server');
+Package.onTest(function (api) {
+  api.use("tinytest");
+  api.use("bslocombe:pg-server");
+  api.use("bslocombe:pg@1.1.8");
+  api.addFiles("pg-server-tests.js", "server");
 });
--- plugin/pgServer.js
+++ plugin/pgServer.js
@@ -1,220 +1,148 @@
 var POSTGRES_STARTUP_TIMEOUT = 10000;
 
-// In Meteor 1.2, the paths to the required tool files has changed,
-// if an error occurs loading the file, try the next set.
-var TOOL_PATHS = [
-  {
-    // Meteor < 1.2
-    fiberHelpers: 'fiber-helpers.js',
-    cleanup: 'cleanup.js'
-  },
-  {
-    // Meteor >= 1.2
-    fiberHelpers: 'utils/fiber-helpers.js',
-    cleanup: 'tool-env/cleanup.js'
-  }
-];
-
-var path = Npm.require('path');
-var fs = Npm.require('fs');
-var Future = Npm.require('fibers/future');
-var pg = Npm.require('pg');
-var spawnSync = Npm.require('spawn-sync');
-var spawn = Npm.require('child_process').spawn;
-var extend = Npm.require('extend');
+var path = Npm.require("path");
+var fs = Npm.require("fs");
+var pg = Npm.require("pg");
+var spawnSync = Npm.require("spawn-sync");
+var spawn = Npm.require("child_process").spawn;
+var extend = Npm.require("extend");
 var defaultConfig = {
-  port: 5432
-}
+  port: 5432,
+};
 
 var postgres;
 var outputStdErr = false;
 var cleanedUp = false;
 var serverReady = false;
 
-function loadMeteorTool(whichTool, index) {
-  var dependency;
-  index = index || 0;
-  try {
-    dependency = Npm.require(path.join(relToolDir, TOOL_PATHS[index][whichTool]));
-  } catch (err) {
-    dependency = loadMeteorTool(whichTool, index + 1);
-  }
-  return dependency;
-}
-
-// With the pg-server-xxx NPM dependency, cannot simply require files from
-//  meteor/tools directory because the Npm.require root directory has changed
-var toolDir = path.dirname(process.mainModule.filename);
-// Assume never more than 100 directories deep
-var rootRelPath = _.range(100).map(function() { return '..' }).join('/');
-// Determine meteor/tools relative directory path
-var relToolDir = path.join(rootRelPath, toolDir);
-
-// For bindEnvironment()
-var fiberHelpers = loadMeteorTool('fiberHelpers');
-var MBE = fiberHelpers.bindEnvironment;
-
 var npmPkg = determinePlatformNpmPackage();
 // Should not happen as package.js should have filtered already
-if(npmPkg === null) return;
+if (npmPkg === null) return;
 
 // Load pg-server-xxx NPM package
-var startServer = function(dataDir, config) {
+var startServer = function (dataDir, config) {
   var fullConfig = extend(defaultConfig, config || {});
 
   try {
     var dataDirStat = fs.statSync(dataDir);
-  } catch(err) {
+  } catch (err) {
     // Data directory does not exist
-    var initResult1 = spawnSync('brew', ['install postgresql@14'])
+    var initResult1 = spawnSync("brew", ["install postgresql@14"]);
     var initResult = spawnSync(
       // path.join(__dirname, 'server/bin/initdb'),
-      'initdb',
-      [ '-D', dataDir, '--username=postgres' ]);
-      if (initResult.status !== 0) {
-        process.stderr.write(initResult.stderr);
-        process.exit(initResult.status);
-      }
+      "initdb",
+      ["-D", dataDir, "--username=postgres"]
+    );
+    if (initResult.status !== 0) {
+      process.stderr.write(initResult.stderr);
+      process.exit(initResult.status);
+    }
   }
 
-  if(dataDirStat && !dataDirStat.isDirectory()) {
-    throw new Error('DATA_DIRECTORY_UNAVAILABLE');
+  if (dataDirStat && !dataDirStat.isDirectory()) {
+    throw new Error("DATA_DIRECTORY_UNAVAILABLE");
   }
 
   // Generate postgresql.conf from provided configuration
-  var conf = Object.keys(fullConfig).map(function(key) {
-      if(fullConfig[key] === null) {
-        return ''
+  var conf = Object.keys(fullConfig)
+    .map(function (key) {
+      if (fullConfig[key] === null) {
+        return "";
       } else {
-        return key + ' = ' + fullConfig[key]
+        return key + " = " + fullConfig[key];
       }
-    }).join('\n');
-  
-  fs.writeFileSync(path.join(dataDir, 'postgresql.conf'), conf);
+    })
+    .join("\n");
 
-  var child = spawn(
-    'postgres', [ '-D', dataDir ]);
-  
-  return child
-}
+  fs.writeFileSync(path.join(dataDir, "postgresql.conf"), conf);
 
-// var startServer = Npm.require(npmPkg);
+  var child = spawn("postgres", ["-D", dataDir]);
+
+  return child;
+};
 
 // Read settings from somefile.pg.json
-Plugin.registerSourceHandler('pg.json', {
-  archMatching: 'os'
-}, function (compileStep) {
-  var settings =
-    loadJSONContent(compileStep, compileStep.read().toString('utf8'));
+Plugin.registerSourceHandler(
+  "pg.json",
+  {
+    archMatching: "os",
+  },
+  function (compileStep) {
+    var settings = loadJSONContent(compileStep, compileStep.read().toString("utf8"));
 
-  // Paths inside the application directory where database is to be stored
-  var dataDir = settings.datadir || '.meteor/local/postgresdb';
-  var dataDirPath = path.join(process.cwd(), dataDir);
+    // Paths inside the application directory where database is to be stored
+    var dataDir = settings.datadir || ".meteor/local/postgresdb";
+    var dataDirPath = path.join(process.cwd(), dataDir);
 
-  if('datadir' in settings) {
-    // dataDir is specified as the first argument to startServer
-    delete settings.datadir;
-  }
+    if ("datadir" in settings) {
+      // dataDir is specified as the first argument to startServer
+      delete settings.datadir;
+    }
 
-  // Determine if data directory is going to be created
-  // This is handled by the dependent NPM package inside startServer
-  try {
-    var dataDirStat = fs.statSync(dataDir)
-  } catch(err) {
-    initializeServer = true;
-  }
-
-  // Start server, but only once, wait for it to be ready (or not)
-  console.log(postgres)
-
-  if(!postgres) {
-    var fut = new Future;
-    postgres = startServer(dataDirPath, settings);
-    // After preset timeout, give up waiting for MySQL to start or fail
-    setTimeout(MBE(function() {
-      if(!fut.isResolved()) {
-        console.log('[ERROR] PostgreSQL startup timeout!             ');
-        fut['return']();
-      }
-    }), POSTGRES_STARTUP_TIMEOUT);
-
-    postgres.stderr.on('data', MBE(function (data) {
-      // Data never used as Buffer
-      data = data.toString();
-      outputStdErr && console.log('[Postgres] ', data);
-
-      // No need to check more if server started already
-      if(fut.isResolved()) return;
-
-      // Check for any known errors
-      var errors = [
-        /could not bind IPv4 socket: Address already in use/,
-        /FATAL: .+/
-      ];
-
-      for(var i = 0; i < errors.length; i++) {
-        var failure = data.match(errors[i]);
-        if(failure !== null) {
-          cleanedUp = true;
-          console.log('[ERROR] ' + failure[0]);
-          return fut['return']();
-        }
-      }
-
-      var ready = data.match(
-        /database system is ready to accept connections/);
-
-      if(ready !== null) {
-        serverReady = true;
-        // Extra spaces for covering Meteor's status messages
-        console.log('=> Started PostgreSQL.                        ');
-        fut['return']();
-      }
-    }));
-
-    return fut.wait();
-  }else{
-    console.log('Postgres already running on:')
-  }
-
-});
-
-// Stop Postgres server on Meteor exit
-loadMeteorTool('cleanup').onExit(function StopPgServer() {
-  if(cleanedUp === false && postgres) {
-    // Only cleanup once!
-    cleanedUp = true;
-
+    // Determine if data directory is going to be created
+    // This is handled by the dependent NPM package inside startServer
     try {
-      postgres.kill();
-    } catch(err) {
-      console.log('[ERROR] Unable to stop PostgreSQL server');
+      var dataDirStat = fs.statSync(dataDir);
+    } catch (err) {
+      initializeServer = true;
+    }
+
+    if (!postgres) {
+      postgres = startServer(dataDirPath, settings);
+
+      postgres.stderr.on("data", function (data) {
+        // Data never used as Buffer
+        data = data.toString();
+        outputStdErr && console.log("[Postgres] ", data);
+
+        // Check for any known errors
+        var errors = [/could not bind IPv4 socket: Address already in use/, /FATAL: .+/];
+
+        for (var i = 0; i < errors.length; i++) {
+          var failure = data.match(errors[i]);
+          if (failure !== null) {
+            cleanedUp = true;
+            console.log("[ERROR] " + failure[0]);
+          }
+        }
+
+        var ready = data.match(/database system is ready to accept connections/);
+
+        if (ready !== null) {
+          serverReady = true;
+          // Extra spaces for covering Meteor's status messages
+          console.log("=> Started PostgreSQL.                        ");
+        }
+      });
+    } else {
+      console.log("Postgres already running on:");
     }
   }
-});
+);
 
 function determinePlatformNpmPackage() {
-  switch(process.platform + '_' + process.arch) {
-    case 'linux_x64': return 'pg-server-9.4-linux-x64';
-    case 'linux_ia32': return 'pg-server-9.4-linux-i386';
-    case 'darwin_x64': return 'pg-server-9.4-osx-x64';
-    case 'darwin_arm64': return 'pg-server-14.2-osx-arm64';
-    default: return null;
+  switch (process.platform + "_" + process.arch) {
+    case "linux_x64":
+      return "pg-server-9.4-linux-x64";
+    case "linux_ia32":
+      return "pg-server-9.4-linux-i386";
+    case "darwin_x64":
+      return "pg-server-9.4-osx-x64";
+    case "darwin_arm64":
+      return "pg-server-14.2-osx-arm64";
+    default:
+      return null;
   }
 }
 
-
-// Begin code borrowed from mquandalle:bower/plugin/handler.js
 var loadJSONContent = function (compileStep, content) {
   try {
     return JSON.parse(content);
-  }
-  catch (e) {
+  } catch (e) {
     compileStep.error({
       message: "Syntax error in " + compileStep.inputPath,
       line: e.line,
-      column: e.column
+      column: e.column,
     });
   }
 };
-// End code from mquandalle:bower
